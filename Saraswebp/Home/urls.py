42a3feae59d6be325e4ca7ab1dbba6b11c68e45b
vamsi krishna <iamvkrishna24@gmail.com>
2025-06-17 15:35:05 +0530

    no home

--- Saraswebp/Home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='home'),  
-    path('login/', views.login_view, name='login'),
-    path('features/', views.features, name='features'),
-    path('testmonials/',views.testmonials,name='testmonials'),
-]
-
